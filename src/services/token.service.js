d81aa8526dfa8abb27cab0a034f51dad297e0878
vlecabr <vitartasleca@gmail.com>
2025-10-31 12:14:18 -0300

    teste

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -1,36 +1,19 @@
-import fs from 'fs';
 import fetch from 'node-fetch';
-import dotenv from 'dotenv';
 import { logInfo, logError } from '../utils/logger.js';
-dotenv.config();
-
-const ENV_PATH = '.env';
-const CLIENT_ID = process.env.ML_CLIENT_ID;
-const CLIENT_SECRET = process.env.ML_CLIENT_SECRET;
+import { ML_CLIENT_ID, ML_CLIENT_SECRET, ML_REFRESH_TOKEN } from '../config/config.js';
 
 let currentAccessToken = null;
-let currentRefreshToken = process.env.ML_REFRESH_TOKEN;
+let currentRefreshToken = ML_REFRESH_TOKEN;
 let expiresAt = 0;
 let refreshTimer = null;
 
-function updateEnvRefreshToken(newToken) {
-  try {
-    const envData = fs.readFileSync(ENV_PATH, 'utf-8');
-    const newEnvData = envData.replace(/^ML_REFRESH_TOKEN=.*$/m, `ML_REFRESH_TOKEN=${newToken}`);
-    fs.writeFileSync(ENV_PATH, newEnvData);
-    logInfo('Novo ML_REFRESH_TOKEN salvo no .env');
-  } catch (err) {
-    logError('Erro ao atualizar o .env', err);
-  }
-}
-
-export async function refreshAccessToken() {
+async function refreshAccessToken() {
   logInfo('Renovando access token...');
   const url = 'https://api.mercadolibre.com/oauth/token';
   const body = new URLSearchParams({
     grant_type: 'refresh_token',
-    client_id: CLIENT_ID,
-    client_secret: CLIENT_SECRET,
+    client_id: ML_CLIENT_ID,
+    client_secret: ML_CLIENT_SECRET,
     refresh_token: currentRefreshToken
   });
 
@@ -41,7 +24,6 @@ export async function refreshAccessToken() {
   });
 
   const data = await res.json();
-
   if (!res.ok) {
     logError('Erro ao renovar token', data);
     throw new Error(data.error_description || 'Erro ao atualizar token');
@@ -50,13 +32,12 @@ export async function refreshAccessToken() {
   currentAccessToken = data.access_token;
   currentRefreshToken = data.refresh_token;
   expiresAt = Date.now() + data.expires_in * 1000;
-  updateEnvRefreshToken(currentRefreshToken);
 
-  // programa a próxima renovação automática
+  // Agenda próxima renovação 1 min antes do fim
   clearTimeout(refreshTimer);
   refreshTimer = setTimeout(refreshAccessToken, data.expires_in * 1000 - 60 * 1000);
 
-  logInfo('Novo access_token obtido! expira em ~6h');
+  logInfo('Novo access_token obtido!');
   return currentAccessToken;
 }
 
